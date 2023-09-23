502ea748fd0457640feb1e8cf9cc0f07d98d0d76
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-23 16:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+console.log("nevil")
+const button=document.getElementById("submit");
+
+
+let signup= document.getElementById("signup-form")
+
+signup.addEventListener("submit",function(event)=>{
+	event.preventDefault();
+	const name=document.getElementById("name").value;
+	const email= document.getElementById("email").value;
+	const password=document.getElementById("password").value;
+	const confirm= document.getElementById("confirm-password").value;
+	console.log(name)
+
+	if(password === confirm){
+		alert("Sign up successful!");
+		sessionstorage.setItem("name", name);
+		sessionstorage.setItem("email", email);
+		sessionstorage.setItem("password",password);
+		
+		
+	}
+	else
+	{
+		alert("Passwords do not match");
+	}
+	
+});
